3bdd33034c4d62584cd8aff59f80055d6284aef3
代长亚 <dai.changya@xcar.com.cn>
2020-04-17 18:57:01 +0800

    add join

---
+++ src/test/java/com/daicy/concurrency/thread/YieldTest.java
@@ -0,0 +1,38 @@
+package com.daicy.concurrency.thread;
+
+/**
+ * @author: create by daichangya
+ * @version: v1.0
+ * @description: com.daicy.concurrency.thread
+ * @date:20-4-17
+ */
+public class YieldTest {
+    public static void main(String[] args) {
+        Thread producer = new Producer();
+        Thread consumer = new Consumer();
+
+        producer.setPriority(Thread.MIN_PRIORITY); //Min Priority
+        consumer.setPriority(Thread.MAX_PRIORITY); //Max Priority
+
+        producer.start();
+        consumer.start();
+    }
+}
+
+class Producer extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I am Producer : Produced Item " + i);
+            Thread.yield();
+        }
+    }
+}
+
+class Consumer extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I am Consumer : Consumed Item " + i);
+            Thread.yield();
+        }
+    }
+}
